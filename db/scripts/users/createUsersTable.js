652eec5dd2c7e63f7e28f7f96ec65710e3978462
Beatriz Bravo <beatrici25@gmail.com>
2022-02-28 16:12:26 +0000

    update users table

--- db/scripts/users/createUsersTable.js
+++ db/scripts/users/createUsersTable.js
@@ -1,7 +1,7 @@
 import db from "../../connection.js";
 
 const response = await db.query(
-  `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, email VARCHAR(60));`
+  `CREATE TABLE IF NOT EXISTS users (user_ID SERIAL PRIMARY KEY, email VARCHAR(60), saved_recipes TEXT[]);`
 );
 
 console.log(response);
