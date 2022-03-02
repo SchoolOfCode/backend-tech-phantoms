81ec8d9c8f10386e9e6c778c63a0c33f1172ec79
Beatriz Bravo <beatrici25@gmail.com>
2022-03-02 16:07:08 +0000

    update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // this environment variable gets handed to us by heroku if we use the postgres add-on
 export const connectionString =
-  process.env.DATABASE_URL === "test"
+  process.env.NODE_ENV === "test"
     ? process.env.TESTING_DATABASE_URL
     : process.env.DATABASE_URL;
+
