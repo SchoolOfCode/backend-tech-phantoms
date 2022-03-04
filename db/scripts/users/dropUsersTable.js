ae83ec170a4759d87e8e9d60ceaeb3260512f2cf
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-03-04 12:05:05 +0000

    Add env to workflow. Slightly update logging.

--- db/scripts/users/dropUsersTable.js
+++ db/scripts/users/dropUsersTable.js
@@ -3,11 +3,11 @@ import db from "../../connection.js";
 
 //FOR DEBUG, DON'T USE UNLESS YOU WANT THE TABLE DEFINITION AND ROWS TO BE GONE
 
-console.log("NODE_ENV", process.env.NODE_ENV);
-console.log("TESTING_DATABASE_URL", typeof process.env.TESTING_DATABASE_URL);
-console.log("DATABASE_URL", typeof process.env.DATABASE_URL);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("TESTING_DATABASE_URL:", typeof process.env.TESTING_DATABASE_URL);
+console.log("DATABASE_URL:", typeof process.env.DATABASE_URL);
 console.log(
-  "Is connectionString TESTING_DATABASE_URL? ",
+  "connectionString === TESTING_DATABASE_URL:",
   process.env.TESTING_DATABASE_URL === connectionString
 );
 
