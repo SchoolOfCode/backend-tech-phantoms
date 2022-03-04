3fe57e8f437958dea27375735e57a4f209358d05
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-03-04 10:47:46 +0000

    Logging envs in dropUsersTable script

--- db/scripts/users/dropUsersTable.js
+++ db/scripts/users/dropUsersTable.js
@@ -2,10 +2,12 @@ import db from "../../connection.js";
 
 //FOR DEBUG, DON'T USE UNLESS YOU WANT THE TABLE DEFINITION AND ROWS TO BE GONE
 
-const response = await db.query(
-    `DROP TABLE IF EXISTS users`
-);
+console.log("NODE_ENV", process.env.NODE_ENV);
+console.log("TESTING_DATABASE_URL", typeof process.env.TESTING_DATABASE_URL);
+console.log("DATABASE_URL", typeof process.env.DATABASE_URL);
 
-console.log(response)
+const response = await db.query(`DROP TABLE IF EXISTS users`);
+
+console.log(response);
 
 db.end();
