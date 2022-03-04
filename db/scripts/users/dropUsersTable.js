8171a7881e8cff1fab2e90c356b8d418e6c22364
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-03-04 11:58:34 +0000

    Logging connectionString

--- db/scripts/users/dropUsersTable.js
+++ db/scripts/users/dropUsersTable.js
@@ -1,3 +1,4 @@
+import { connectionString } from "../../../config.js";
 import db from "../../connection.js";
 
 //FOR DEBUG, DON'T USE UNLESS YOU WANT THE TABLE DEFINITION AND ROWS TO BE GONE
@@ -5,6 +6,10 @@ import db from "../../connection.js";
 console.log("NODE_ENV", process.env.NODE_ENV);
 console.log("TESTING_DATABASE_URL", typeof process.env.TESTING_DATABASE_URL);
 console.log("DATABASE_URL", typeof process.env.DATABASE_URL);
+console.log(
+  "Is connectionString TESTING_DATABASE_URL? ",
+  process.env.TESTING_DATABASE_URL === connectionString
+);
 
 const response = await db.query(`DROP TABLE IF EXISTS users`);
 
